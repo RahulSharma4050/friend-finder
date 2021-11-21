e1c9f90a9cac3c2969110054d8983d8c92a8704e
RahulSharma4050 <94781889+RahulSharma4050@users.noreply.github.com>
2021-11-21 15:13:51 +0530

    Initial commit

---
+++ db.php
@@ -0,0 +1,12 @@
+<?
+define('DB_HOST','localhost');
+define('DB_USER',root);
+define('DB_PASSWORD',root);
+define('DB_NAME',dbsocial);
+
+$con = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) 
+           or die(mysqli_connect_error() );
+		   
+mysqli_select_db( $con,DB_NAME ) or die(mysqli_error($con));	
+
+?>
